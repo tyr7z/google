6e214fa5f415af4fae127d411fc7bb2823922497
tyr7z <tyr7z@users.noreply.github.com>
2025-04-10 15:37:25 +0200

    nested mkdir

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir ./internal/build
 go build -o ./internal/build/ ./internal/...
-mkdir $PREFIX/share/google/play
+mkdir -p $PREFIX/share/google/play
 cp ./internal/build/* $PREFIX/share/google/play
