861ee3738c3e72cfab459885b283eb5f5f5670d0
tyr7z <tyr7z@users.noreply.github.com>
2025-04-10 15:32:06 +0200

    update setup scripts

--- setup-windows.bat
+++ setup-windows.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir .\internal\build
 go build -o ./internal/build/ ./internal/...
-mkdir C:\Users\%USERNAME%\google-play
+mkdir C:\Users\%USERNAME%\google\play
 copy .\internal\build\* C:\tools\bin\
